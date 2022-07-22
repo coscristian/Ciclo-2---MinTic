8dd0f2fb34669af1cd15f4d42640dc5bd502b0eb
Cristian Quesada Cossio <crisquesadaco@gmail.com>
2022-07-21 23:52:46 -0500

    Commit avance JDBC

--- ProyectosCiclo2/demojdbc/src/main/java/com/cristian/desarrollo/App.java
+++ ProyectosCiclo2/demojdbc/src/main/java/com/cristian/desarrollo/App.java
@@ -1,37 +1,40 @@
 package com.cristian.desarrollo;
 
+import com.cristian.desarrollo.dto.BaseDatos;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class App {
     public static void main( String[] args ){
         var baseDatos = new BaseDatos();
-        var archivoDB = "//home//coscristian//Documentos//MisionTic 2022//Ciclo 2//MisionTIC-2022-R2-Ciclo2-Programacion_Basica//clase08//assets//hr.db";
+        var archivoDB = "//home//coscristian//Escritorio//Ciclo-2---MinTic//BBDD//SQL//hr.db";
         try{
+            // 1. Realizar conexión con la BBDD
             var connection = baseDatos.getConnection("jdbc:sqlite:" + archivoDB);
             System.out.println("Se ha realizado la conexión a la BBDD");
+            
+            // 2. Permitir realizar comandos SQL
+            var statement = connection.createStatement();
+            
+            // 3. Añadir sentencias SQL
+            // statement.execute("CREATE TABLE DEMO (ID INTEGER NOT NULL, NAME VARCHAR(200) NOT NULL, CONSTRAINT DEMO_PK PRIMARY KEY(ID));");
+            // statement.execute("insert into demo (id, name) values (1, 'Datos iniciales');");
+            // statement.execute("insert into demo (id, name) values (2, 'Datos adicionales');");
+            
+            // Traer datos y mostrar datos
+            ResultSet rset = statement.executeQuery("SELECT * FROM DEMO;");
+            while(rset.next()){
+                System.out.printf("ID: %d \t", rset.getInt("ID"));
+                System.out.printf("NAME: %s \t%n", rset.getString("NAME"));
+            }
+            // statement.execute("DROP TABLE DEMO;");
 
 
-
+            // Final --> Cerrar conexión con BBDD
             baseDatos.closeConnection();
         }catch(SQLException ex){
-            System.err.println(ex);
+            System.err.println("Existe un probema al trabajar con la base de datos: \n" + ex.getMessage());
         }
     }
-
-    public void nombre(){
-        var statement = connection.createStatement(); // Instrucciones
-        statement.execute("CREATE TABLE DEMO (ID INTEGER NOT NULL, NAME VARCHAR(200) NOT NULL, CONSTRAINT DEMO_Pk");
-        statement.execute("insert into demo (id,name) values (1, 'Datos iniciales');");
-
-        var rset = statement.executeQuery("SELECT * FROM DEMO"); // rset: Conjunto de datos resultantes, es un flujo de datos, por lo tanto hay que cerrarlo
-        while(rset.next()){ // recorre todos los elementos de la consulta
-            System.out.printf("ID: %d \t", rset.getInt("ID"));
-            System.out.printf("NAME: %s %n", rset.getString("NAME"));
-
-        }
-
-        rset.close();
-    }
-
-
 }
--- ProyectosCiclo2/demojdbc/src/main/java/com/cristian/desarrollo/dto/BaseDatos.java
+++ ProyectosCiclo2/demojdbc/src/main/java/com/cristian/desarrollo/dto/BaseDatos.java
@@ -13,29 +13,15 @@ public class BaseDatos {
 
     public Connection getConnection(String url, String username, String password) throws SQLException{
         if (conn == null){
-            //Pedir la conexión
+            //Pedir la conexión: Driver Manager permite dar la conexión dada la URL
             conn = DriverManager.getConnection(url, username, password);
         }
         return conn;
-    }
+    }  
 
     public void closeConnection() throws SQLException{
         if(conn != null){
             conn.close();
         }
-    }
-
-    public List<Departments> listarDepartamentos(){
-        var resp = new ArrayList<Departments>();
-        var statement = new Statement();
-
-        // Listar Departamentos : Hacer consultaa en BBDD
-        
-
-        return resp;
-    }
-
-    public List<Jobs> listarCargos() throws Exception {
-
     }
 }
